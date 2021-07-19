a73273629369100bcb27e0f517c28b20ac2ff1e7
Keesuk Lee <keesukee@naver.com>
2021-07-19 18:06:28 +0900

    pagemodules

---
+++ .history/src/pages/_HomePage_20210719180506.js
@@ -0,0 +1,35 @@
+import { portFolioContents } from "../_data/_Data.jsx";
+
+import { MainLayout } from "../components/_Layout.jsx";
+import { MainTitleDiv, LinkA } from "../components/Main/Title.jsx";
+import FolioList from "../components/Main/FolioList.jsx";
+import { welcomeMention, otherSiteLink } from "../_data/_Data.jsx";
+
+
+const HomePage = () => {
+    return(
+        <MainLayout>
+            <MainTitleDiv default>
+                <div className="text">
+                    {welcomeMention}
+                    <span className="link">
+                        {otherSiteLink.map(v =>
+                            <LinkA 
+                                color={v["color"]} 
+                                href={v["link"]} 
+                                key={v["title"]}
+                            >
+                                {v["title"]}
+                            </LinkA>)
+                        }
+                    </span>
+                </div>
+            </MainTitleDiv>
+            <FolioList 
+                folioArr={portFolioContents}
+            />
+        </MainLayout>
+    )
+}
+
+export default HomePage;
