139be44186a70f9ba26c7e3b48ae600901d5b882
Keesuk Lee <keesukee@naver.com>
2021-07-26 16:40:34 +0900

    translate

---
+++ .history/src/pages/Seoul_20210726144803.js
@@ -0,0 +1,56 @@
+import { useRef, createRef, useEffect } from "react";
+
+import { numbering } from "../_data/_Functions.jsx";
+import { portFolioContents, SeoulContents } from "../_data/_Data.jsx";
+
+import pageModules from "../components/Main/PageModules.jsx";
+import { MainLayout } from "../components/_Layout.jsx";
+import { MainTitleDiv } from "../components/Main/Title.jsx";
+import Footnote from "../components/Main/Footnote.jsx";
+import FolioList from "../components/Main/FolioList.jsx";                                
+
+
+
+const _Seoul = () => {
+    const { name, pageStyle, mark, title, category, color, logo, mainText, subText, image } = SeoulContents
+    const { SubTitle, TitleBoxLogo, Remark, MainText, SubText, OuterImgBox, InnerImgBoxBackground, InnerImgBox, MarkBox, } = pageModules
+
+    const scrRef = useRef()
+    const styRefs = useRef([])    
+    styRefs.current = pageStyle.map(i => styRefs.current[i] = createRef())
+    
+    useEffect(() => {
+        
+    }, [])
+
+    return(
+        <MainLayout
+            ref={scrRef}
+        >
+            <MainTitleDiv
+                color={color["sub"]}
+                TorB={color["TorB"]}
+            >
+                <div className="text">
+                    <mark>{title}</mark><br/>
+                    {category}
+                </div>
+            </MainTitleDiv> 
+            <TitleBoxLogo 
+                backGroundColor={color["sub"]}
+                logoTypeColor={"white"}
+                logoBackColor={"black"}
+                paddingTopBottom={"11"}
+                logoWidth={"40"}
+            >
+                <div className="logoBox">
+                    <div className="logo">
+                        {logo}
+                    </div>
+                </div>
+            </TitleBoxLogo>
+        </MainLayout>
+    )
+}
+
+export default _Seoul;
