d757c1552dfbb92b72db17b3acd6ad714e64971e
Keesuk Lee <keesukee@naver.com>
2021-08-01 18:34:01 +0900

    magnifier

---
+++ .history/src/components/Main/LogoBox_20210801173827.jsx
@@ -0,0 +1,138 @@
+import { useState, useEffect, useRef} from "react";
+import styled from "styled-components";
+import { useWindowSize } from "../../_data/_Functions.jsx";                              
+import ReactCursorPosition from "react-cursor-position";
+
+const TitleBoxLogoDiv = styled.div`
+    background-color: ${({backGroundColor}) => backGroundColor};
+    margin-left: 2.5vw;
+    margin-top: 2.5vw;
+    width: calc(100%-2.5vw);
+    padding: ${({paddingTopBottom}) => paddingTopBottom}% 5%;
+    transition: all .2s;
+
+    .logo {
+        pointer-events: auto;
+        .logoType {
+            fill: ${({logoTypeColor}) => logoTypeColor};
+    }}
+    &:hover {
+        background-color: ${({logoTypeColor}) => logoTypeColor};
+        .logoType { 
+            fill: ${({logoBackColor}) => logoBackColor};
+    }}
+`;
+
+const CursorDiv = styled.div`
+    position: absolute;
+    z-index: 100000000;
+    transform: translate(${({cor}) => `${100}px, ${400}px`}) scale(1.0);
+    opacity: ${({active}) => active === true ? "1" : "0"};
+    background-color: white;
+    clip-path: circle(4.4rem at left top);
+    pointer-events: none;
+    transition: all .05s ease;
+    transition-property: background-color, opacity, transform, width, height;
+
+    &:after {
+        content: "";
+        display: flex;
+        position: absolute;
+        z-index: 1000;
+        left: -4.4rem;
+        top: -4.4rem;
+        background-color: white;
+        width: 8rem;
+        height: 8rem;
+        border-radius: 8rem;
+        border: .4rem solid black;
+    }
+`;
+const LogoMask = styled.div`
+    pointer-events: auto;
+    position: absolute;
+    z-index: 10000;
+    width: ${({width}) => width*2}px;
+    height: ${({height}) => height*2}px;
+    .logoType {
+        fill: ${({logoTypeColor}) => logoTypeColor};
+    }
+`;
+
+const TitleBoxLogo = ({
+    position: {x, y}, 
+    detectedEnvironment: {isMouseDetected, isTouchedDected},
+    isActive,
+    conRef,
+    logo }) => {
+
+    const [position, setPosition] = useState({ x: 0, y: 0 })
+    const [clicked, setClicked] = useState(false)
+    const [linkHovered, setLinkHovered] = useState(false)
+    const [active, setActive] = useState(false)
+    const [imgCor, setImgCor] = useState({})
+
+    const imgRef = useRef()
+
+    useEffect(() => {
+        const cor = imgRef.current.getBoundingClientRect()
+        setImgCor(cor)
+    }, [])
+
+    useEffect(() => {
+        setPosition({ x: x, y: y })
+    }, [x, y])
+
+    return (
+        <>
+            <CursorDiv 
+                active={isActive}
+                cor={imgCor}
+                style={{ left: `${position.x}px`,top: `${position.y}px`}}
+            >
+                <LogoMask
+                    width={imgCor.width}
+                    height={imgCor.height}
+                    left={imgCor.left}
+                    top={imgCor.top}
+                    style={{ transform: `translate(-${position.x*2}px, -${position.y*2}px)`}}
+                >
+                    {logo}
+                </LogoMask>
+            </CursorDiv>
+            <div 
+                className="logo"
+                ref={imgRef}
+            >
+                {logo}
+            </div>
+        </>
+    )
+}
+
+const LogoBox = ({ backGroundColor, logoTypeColor, logoBackColor, paddingTopBottom, logoWidth, logo, }) => {
+    const conRef = useRef()
+
+    return (
+        <TitleBoxLogoDiv 
+            ref={conRef}
+            backGroundColor={backGroundColor}
+            logoTypeColor={logoTypeColor}
+            logoBackColor={logoBackColor}
+            paddingTopBottom={paddingTopBottom}
+            logoWidth={logoWidth}
+        >   
+            <ReactCursorPosition style={{cursor: "none", pointerEvents: "none", padding: "20px"}}>
+                <TitleBoxLogo con={conRef} logo={logo}/>
+            </ReactCursorPosition>
+        </TitleBoxLogoDiv>
+    )
+}
+
+export default LogoBox;
+
+
+
+
+    
+    
